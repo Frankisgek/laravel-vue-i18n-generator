0510a68bdc587893b50e770fbf77cd1040f99f84
Frank <frank.keulen@cerios.com>
2026-09-02 09:34:41 +0200

    PHPcs fix

--- src/Console/GenerateVueTranslations.php
+++ src/Console/GenerateVueTranslations.php
@@ -118,7 +118,7 @@ class GenerateVueTranslations extends Command
     {
         return match (pathinfo($filename, PATHINFO_EXTENSION)) {
             'json' => json_decode(file_get_contents($filename), true),
-            'php' => [basename($filename, '.php') => include($filename)],
+            'php' => [basename($filename, '.php') => include ($filename)],
         };
     }
 
@@ -198,7 +198,6 @@ class GenerateVueTranslations extends Command
     /**
      * Writes translations to a JSON file.
      *
-     * @param string $filename
      * @param array<string,array> $translations
      */
     protected function generateVue18nFile(string $filename, array $translations): int|false
